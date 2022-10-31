324df1f341c6280ac8b5ece4cfedff8c239823b0
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-10-31 14:23:54 -0400

    commit tree 38ac364663addbaa9d9f4f715db0b8883b0b1f91

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -127,3 +128,34 @@ def game_hash
 end
 
 # Write code here
+def all_players
+  game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+def player_stats player_name
+  all_players.find do |player| 
+    player[:player_name] == player_name 
+  end
+end
+
+def num_points_scored player_name
+  player = player_stats(player_name)
+  player[:points]
+end
+
+puts num_points_scored "Jeff Adrien"
+
+def shoe_size player_name
+  player = player_stats(player_name)
+  player[:shoe]
+end
+
+def teams
+  '#{game_hash[:home][:team_name]}' + '#{game_hash[:away][:team_name]}'
+end
+
+def team_names team_name
+  teams.find do |team|
+    team[:team_name] == team_name
+  end
+end
